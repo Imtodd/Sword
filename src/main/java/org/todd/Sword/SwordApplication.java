ccb8f96b3b81e5896b9b5dccb57d586b39ac6082
邢照珺 <542862986@qq.com>
2017-09-22 08:41:43 +0800

    Initial commit

---
+++ src/main/java/org/todd/Sword/SwordApplication.java
@@ -0,0 +1,12 @@
+package org.todd.Sword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwordApplication.class, args);
+	}
+}
