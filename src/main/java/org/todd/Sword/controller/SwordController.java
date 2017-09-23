7b6dc9e6b7f4d71fc80f120fada71fc69262c3bc
邢照珺 <542862986@qq.com>
2017-09-23 09:15:44 +0800

    使用spring的依赖注入
    实现一个调用工具的小例子

--- src/main/java/org/todd/Sword/controller/SwordController.java
+++ src/main/java/org/todd/Sword/controller/SwordController.java
@@ -10,18 +10,29 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.todd.Sword.po.Person;
 import org.todd.Sword.service.ISwordService;
+import org.todd.Sword.tools.DemoTools;
 
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 
 @RestController
-@RequestMapping(value="sword")
 public class SwordController {
 
 	@Autowired
 	private ISwordService service;
 	
 	
+	@Autowired
+	private DemoTools tools;
+	
+	@ApiOperation(value="项目拦截url",notes="作为项目拦截路径")
+	@RequestMapping(value="/",method=RequestMethod.GET)
+	@ResponseBody
+	public String index() {
+		return "lalala";
+	}
+	
+	@ApiOperation(value="初始化新增一条人物数据",notes="初始化程序")
 	@RequestMapping(value="init",method=RequestMethod.GET)
 	@ResponseBody
 	public String init() {
@@ -40,16 +51,14 @@ public class SwordController {
 		return service.getone(id).toString(); 
 	}
 	
-//	public Person getAll() {
-//		return service.
-//		
-//	}
-	
-	@RequestMapping(value="home",method=RequestMethod.GET)
-	public String home(Model model) {
-		model.addAttribute("name", "todd");
-		return "index";
+	@ApiOperation(value="执行工具类去做某事",notes="去做某事")
+	@RequestMapping(value="todo",method=RequestMethod.GET)
+	@ResponseBody
+	public String todo() {
+		return tools.todoSomething();
 	}
+	
+	
 	
 	
 	
---
+++ src/main/java/org/todd/Sword/tools/DemoTools.java
@@ -0,0 +1,12 @@
+package org.todd.Sword.tools;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class DemoTools {
+
+	public String todoSomething() {
+		return "你好，我正在忙着做某事";
+	}
+	
+}
