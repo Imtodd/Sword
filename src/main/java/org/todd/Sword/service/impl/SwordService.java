0a74dc553e802204a0bdd31ab09bc1f0343f5a14
邢照珺 <542862986@qq.com>
2017-09-23 17:52:56 +0800

    使用logback
    logback日志功能

--- src/main/java/org/todd/Sword/service/impl/SwordService.java
+++ src/main/java/org/todd/Sword/service/impl/SwordService.java
@@ -1,5 +1,7 @@
 package org.todd.Sword.service.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.todd.Sword.po.Person;
@@ -9,11 +11,15 @@ import org.todd.Sword.service.ISwordService;
 @Service
 public class SwordService implements ISwordService {
 	
+	//定义一个全局的记录器，通过LoggerFactory获取
+    private final static Logger logger = LoggerFactory.getLogger(SwordService.class); 
+	
 	@Autowired
 	private IPersonRespository personRespository;
 
 	@Override
 	public Person getone(int id) {
+		logger.info("我是日志");
 		return personRespository.findOne(id);
 	}
 
